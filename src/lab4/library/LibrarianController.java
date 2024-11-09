2e542afdd824314e161b04b1d516795b5d07e31b
Nick <dragonlordbest666@gmail.com>
2024-11-08 17:35:56 -0800

    LibrarianController
    Done!

--- src/lab4/library/LibrarianController.java
+++ src/lab4/library/LibrarianController.java
@@ -5,7 +5,32 @@ public class LibrarianController {
 	// GRASP Principle: Controller
 	// Handles user requests like borrowing and returning books.
 	// Delegates tasks to the appropriate objects.
+	private Library library;
 	
-	// TODO: implement functionality of Member class
+	// DONE: implement functionality of Member class
+	public LibrarianController(Library library) {
+		this.library = library;
+	}
+	  public void borrowBook(String memberName, String bookTitle) {
+	        Member member = library.findMemberByName(memberName);
+	        Book book = library.findBookByTitle(bookTitle);
+	        
+	        //if member and book is existing
+	        if (member != null && book != null) {
+	            member.borrowBook(book);
+	        } else {
+	            System.out.println("Member or book not found.");
+	        }
+	    }
 
-}
+	    public void returnBook(String memberName, String bookTitle) {
+	        Member member = library.findMemberByName(memberName);
+	        Book book = library.findBookByTitle(bookTitle);
+
+	        if (member != null && book != null) {
+	            member.returnBook(book);
+	        } else {
+	            System.out.println("Member or book not found.");
+	        }
+	    }
+	}
