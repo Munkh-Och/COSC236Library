dc00617618f24e70104eaa0ae461273c2f540cf6
Munkh-Och <z.munkh.och2023@gmail.com>
2024-11-08 17:38:28 -0800

    test1

--- src/lab4/library/Library.java
+++ src/lab4/library/Library.java
@@ -1,5 +1,8 @@
 package lab4.library;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
 	
 	//  GRASP Principles: Creator, Controller
@@ -10,7 +13,40 @@ public class Library {
 	// Attributes:
 	// private catalog (list of Book) 
 	// private members (list of Members)
+	private List<Book> catalog;
+	private List<Member> members;
 
 	// TODO: implement functionality of Member class
-
+	public Library() {
+		catalog = new ArrayList<>();
+		members = new ArrayList<>();
+	}
+	
+	public void addBook(Book book) {
+		catalog.add(book);
+		System.out.println(book.getTitle() + " added to the library catalog");
+	}
+	
+	public void registerMember(Member member) {
+		members.add(member);
+		System.out.println(member.getName() + " registered as a library member.");
+	}
+	
+	public Book findBookByTitle(String title) {
+        for (Book book : catalog) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+	
+	public Member findMemberByName(String name) {
+        for (Member member : members) {
+            if (member.getName().equalsIgnoreCase(name)) {
+                return member;
+            }
+        }
+        return null;
+    }
 }
--- src/lab4/library/Member.java
+++ src/lab4/library/Member.java
@@ -1,5 +1,8 @@
 package lab4.library;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
 
 	// GRASP Principle: Information Expert and Low Coupling
@@ -7,7 +10,32 @@ public class Member {
 
 	private String name;
 	// private borrowedbBooks TODO: implement collection of borrowed books
-	
+	private List<Book> borrowedBooks;
 	// TODO: implement functionality of Member class
+	public Member(String name) {
+		this.name = name;
+		this.borrowedBooks = new ArrayList<>();
+	}
+	
+	public void borrowBook(Book book) {
+		if(book.isAvailable()) {
+			book.setAvailable(false);
+			borrowedBooks.add(book);
+			System.out.println(name + " borrowed " + book.getTitle());
+		} else {
+			System.out.println(book.getTitle() + " is not available");
+		}
+	}
+	
+	public void returnBook(Book book) {
+		if(borrowedBooks.remove(book)) {
+			book.setAvailable(true);
+			System.out.println(name + " returned " + book.getTitle());
+		}
+	}
+	
+	public String getName() {
+		return name;
+	}
 
 }
