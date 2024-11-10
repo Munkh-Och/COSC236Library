b1836167f81ff7c758f60be78a8f0fa780af1515
Munkh-Och <z.munkh.och2023@gmail.com>
2024-11-10 15:42:45 -0800

    Library Management System

--- src/lab4/library/Book.java
+++ src/lab4/library/Book.java
@@ -4,7 +4,6 @@ public class Book {
 
 	// GRASP Principle: Information Expert
 	// Manages its own state (available or borrowed).
-	//.
 	private String title;
 	private String author;
 	private boolean isAvailable; 
